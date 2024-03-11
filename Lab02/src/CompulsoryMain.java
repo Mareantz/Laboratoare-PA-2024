614e1240fe33be663495f307a5f35f0a0377d25f
Mareantz <marianciotir@gmail.com>
2024-03-11 11:33:24 +0200

    added readme for homework part

---
+++ Lab02/src/CompulsoryMain.java
@@ -0,0 +1,20 @@
+import VehicleRoutingProblem.Client;
+import VehicleRoutingProblem.Depot;
+import VehicleRoutingProblem.Vehicle;
+
+import java.time.LocalTime;
+
+public class CompulsoryMain
+{
+    public void Compulsory()
+    {
+        Depot depot = new Depot("Depot 1", 5, 10, 10, 20, 1);
+        System.out.println(depot);
+
+        Vehicle vehicle = new Vehicle(depot, true);
+        System.out.println(vehicle);
+
+        Client client = new Client("Client 1", LocalTime.of(9, 0), LocalTime.of(10, 0));
+        System.out.println(client);
+    }
+}
--- Lab02/src/Main.java
+++ Lab02/src/Main.java
@@ -13,17 +13,6 @@ import java.util.List;
 
 public class Main
 {
-//    public void Compulsory()
-//    {
-//        Depot depot = new Depot("Depot 1", 5, 10, 10, 20, 1);
-//        System.out.println(depot);
-//
-//        Vehicle vehicle = new Vehicle(depot, true);
-//        System.out.println(vehicle);
-//
-//        Client client = new Client("Client 1", LocalTime.of(9, 0), LocalTime.of(10, 0));
-//        System.out.println(client);
-//    }
     public static void main(String[] args)
     {
         Depot depot1 = new Depot("Depot 1", 0, 0, 10, 10, 1);
